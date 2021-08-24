10ada752417a9ab5cdaae94f8ff2b1e64cf1494b
Ali Hassan <alihassan17122002@gmail.com>
2021-08-23 19:07:55 -0800

    Created the DB and Dao.

---
+++ app/src/main/java/com/example/supertodolist/data/TaskDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.supertodolist.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database (entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase(){
+
+    abstract fun taskDao(): TaskDao
+
+}
