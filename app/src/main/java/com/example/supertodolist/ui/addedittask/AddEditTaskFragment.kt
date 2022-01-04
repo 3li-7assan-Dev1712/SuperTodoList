439652cc0f7ad30d6d9f25da7140cc3768667141
Ali Hassan <alihassan17122002@gmail.com>
2022-01-03 21:03:19 -0800

    start implementing the snackbar feedback to make confermation message to inform the user.

--- app/src/main/java/com/example/supertodolist/ui/addedittask/AddEditTaskFragment.kt
+++ app/src/main/java/com/example/supertodolist/ui/addedittask/AddEditTaskFragment.kt
@@ -2,13 +2,20 @@ package com.example.supertodolist.ui.addedittask
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.supertodolist.R
 import com.example.supertodolist.databinding.FragmentAddEditTaskBinding
+import com.example.supertodolist.util.exhaustive
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class AddEditTaskFragment: Fragment(R.layout.fragment_add_edit_task) {
@@ -35,6 +42,24 @@ class AddEditTaskFragment: Fragment(R.layout.fragment_add_edit_task) {
             }
             fabSaveTask.setOnClickListener {
                 viewModel.onFabSaveTaskClick()
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.addEditTaskEvents.collect {event ->
+                when (event) {
+                    is AddEditTaskViewModel.AddEditTaskEvents.NavigateBackWithResult -> {
+                        binding.editTextTaskName.clearFocus()
+                        setFragmentResult(
+                            "add_edit_task_request",
+                            bundleOf("add_edit_task_result_flag" to event.result)
+                        )
+                        findNavController().popBackStack()
+                    }
+
+                    is AddEditTaskViewModel.AddEditTaskEvents.ShowInvalidMessage -> {
+                        Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_LONG).show()
+                    }
+                }.exhaustive
             }
         }
     }
--- app/src/main/java/com/example/supertodolist/ui/addedittask/AddEditTaskViewModel.kt
+++ app/src/main/java/com/example/supertodolist/ui/addedittask/AddEditTaskViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.supertodolist.ui.addedittask
 
+import android.app.Activity
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
@@ -7,15 +8,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.supertodolist.data.Task
 import com.example.supertodolist.data.TaskDao
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+
+
+const val ADD_TASK_OK = Activity.RESULT_FIRST_USER
+const val EDIT_TASK_OK = Activity.RESULT_FIRST_USER + 1
 
 class AddEditTaskViewModel @ViewModelInject constructor(
     val taskDao: TaskDao,
     @Assisted private val state: SavedStateHandle
-): ViewModel() {
+) : ViewModel() {
+    val addEditTasksEventsChannel = Channel<AddEditTaskEvents>()
+    val addEditTaskEvents = addEditTasksEventsChannel.receiveAsFlow()
+
     fun onFabSaveTaskClick() {
         if (taskName.isBlank()) {
             // show input valid message
+            showInvalidMessage("name cannot be empty")
         }
         if (task != null) {
             // update the task
@@ -24,22 +35,28 @@ class AddEditTaskViewModel @ViewModelInject constructor(
             // navigate back
         } else {
             // create new task
-            val newTask = Task(name  = taskName, important = taskImportance)
+            val newTask = Task(name = taskName, important = taskImportance)
             addNewTask(newTask)
             // navigate back
         }
+    }
+
+    private fun showInvalidMessage(msg: String) = viewModelScope.launch {
+        addEditTasksEventsChannel.send(AddEditTaskEvents.ShowInvalidMessage(msg))
     }
 
     private fun updateTask(updatedTask: Task) {
 
         viewModelScope.launch {
             taskDao.update(updatedTask)
+            addEditTasksEventsChannel.send(AddEditTaskEvents.NavigateBackWithResult(EDIT_TASK_OK))
         }
     }
 
     private fun addNewTask(newTask: Task) {
         viewModelScope.launch {
             taskDao.insert(newTask)
+            addEditTasksEventsChannel.send(AddEditTaskEvents.NavigateBackWithResult(ADD_TASK_OK))
         }
 
     }
@@ -51,9 +68,13 @@ class AddEditTaskViewModel @ViewModelInject constructor(
             state.set("taskName", value)
         }
     var taskImportance = state.get<Boolean>("taskImportance") ?: task?.important ?: false
-    set(value) {
-        field = value
-        state.set("taskImportance", value)
-    }
+        set(value) {
+            field = value
+            state.set("taskImportance", value)
+        }
 
+    sealed class AddEditTaskEvents {
+        data class ShowInvalidMessage(val msg: String) : AddEditTaskEvents()
+        data class NavigateBackWithResult(val result: Int) : AddEditTaskEvents()
+    }
 }
